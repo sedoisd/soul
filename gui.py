cdcd5bcab5e3bd2bcc75d2b5de2746dc8992d843
sd <sarasckin.serafim@yandex.ru>
2025-01-07 08:49:36 +0400

    в набросок добавлены image и name - weapon и characters

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from pygame import Rect
 import pygame
 import pygame_gui
+from pygame_gui.elements import *
 from constants import SIZE
 import os
 import sys
@@ -37,29 +38,46 @@ class GuiManager:
             self.button_shop.kill()
             self.button_setting.kill()
             self.button_exit.kill()
+            self.image_character = None
+            self.label_image_character.kill()
+            self.label_name_character.kill()
+            self.image_weapon = None
+            self.label_image_weapon.kill()
+            self.label_name_weapon.kill()
             command_load()
 
         size_button = (200, 70)
-        self.button_start = pygame_gui.elements.UIButton(relative_rect=Rect((30, 180, *size_button)),
-                                                         text='Старт', manager=self.manager, command=start)
-        self.button_shop = pygame_gui.elements.UIButton(relative_rect=Rect((30, 270, *size_button)),
-                                                        text='Магазин', manager=self.manager,
-                                                        command=lambda: redirection(self.load_shop))
-        self.button_setting = pygame_gui.elements.UIButton(relative_rect=Rect((30, 360, *size_button)),
-                                                           text='Настройки', manager=self.manager,
-                                                           command=lambda: redirection(self.load_setting))
-        self.button_exit = pygame_gui.elements.UIButton(relative_rect=Rect((30, 450, *size_button)),
-                                                        text='Выход', manager=self.manager)
+        self.button_start = UIButton(relative_rect=Rect((30, 180, *size_button)),
+                                     text='Старт', manager=self.manager, command=start)
+        self.button_shop = UIButton(relative_rect=Rect((30, 270, *size_button)),
+                                    text='Магазин', manager=self.manager,
+                                    command=lambda: redirection(self.load_shop))
+        self.button_setting = UIButton(relative_rect=Rect((30, 360, *size_button)),
+                                       text='Настройки', manager=self.manager,
+                                       command=lambda: redirection(self.load_setting))
+        self.button_exit = UIButton(relative_rect=Rect((30, 450, *size_button)),
+                                    text='Выход', manager=self.manager)
 
-        # self.sprite_character = pygame.sprite.Sprite()
+        # Character menu
+        self.label_image_character = UILabel(Rect((650, 0, 150, 150)), text='')
+        self.image_character = pygame.image.load('image/test_pers.png').subsurface((53, 0, 52, 72))
+        self.image_character = pygame.transform.rotozoom(self.image_character, 0, 4)
+        self.label_image_character.set_image(self.image_character)
+        self.label_name_character = UILabel(Rect((690, 310, 100, 30)), text='Рыцарь')
+
+        # Weapon menu
+        self.label_image_weapon = UILabel(Rect((350, 60, 150, 100)), text='')
+        self.image_weapon = pygame.image.load('image/weapon.png')
+        self.label_image_weapon.set_image(self.image_weapon)
+        self.label_name_weapon = UILabel(Rect((410, 315, 60, 30)), text='Меч')
 
     def load_setting(self):
         def back():
             self.button_back.kill()
             self.load_start_menu()
 
-        self.button_back = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(2, 2, 40, 40), text='back',
-                                                        manager=self.manager, command=back)
+        self.button_back = UIButton(relative_rect=pygame.Rect(2, 2, 40, 40), text='back',
+                                    manager=self.manager, command=back)
         # image_button_back = pygame.image.load('image/back.png')
         # self.button_back._set_image(image_button_back)
 
@@ -68,5 +86,5 @@ class GuiManager:
             self.button_back.kill()
             self.load_start_menu()
 
-        self.button_back = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(2, 2, 40, 40), text='back',
-                                                        manager=self.manager, command=back)
+        self.button_back = UIButton(relative_rect=pygame.Rect(2, 2, 40, 40), text='back',
+                                    manager=self.manager, command=back)
