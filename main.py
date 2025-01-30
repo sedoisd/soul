395634bd667e57dd99a2db0194d4ed6416c14af3
Rinard Ahmetzyanov <rinardahetzyanov@yandex.ru>
2025-01-30 23:04:44 +0300

    updates

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame_gui
 from constants import *
 from managers import GuiManager, DatabaseManager
 from initialization_classes import Character
-from other_functions import get_frame_current_background
+from other_functions import get_frame_current_background, get_weapon_settings, update_setting, buy
 
 # import sys
 
@@ -17,7 +17,8 @@ class Game:
         # init classes
         self.gui_manager = GuiManager()
         self.database_manager = DatabaseManager()
-        self.progress_bar1, self.progress_bar2 = self.database_manager.get_characteristics_settings()
+        self.money, self.progress_bar1, self.progress_bar2, self.characters, self.weapon = get_weapon_settings()
+        self.money, self.progress_bar1, self.progress_bar2 = int(self.money), int(self.progress_bar1), int(self.progress_bar2)
         self.progress_bar1_f, self.progress_bar2_f = self.progress_bar1, self.progress_bar2
         self.clock = pygame.time.Clock()
 
@@ -30,6 +31,7 @@ class Game:
         self.flag_going_shop = False
         self.flag_going_setting = False
         self.flag_going_game1 = False
+        self.place = 'menu'
         self.player = None
         self.time_delta = None
         self.music_menu = pygame.mixer.music.load("sound/music/menu.mp3")
@@ -53,79 +55,94 @@ class Game:
         self.gui_manager.manager.process_events(event)
         if event.type == pygame.QUIT or (
                 event.type == pygame_gui.UI_BUTTON_PRESSED and event.ui_element == self.gui_manager.button_exit):
-            
             self.running = False
         if event.type == pygame.MOUSEBUTTONUP:
             print(event.pos)
             if self.flag_going_game:
                 self.player.rect.x, self.player.rect.y = event.pos
-        try:
             if event.type == pygame_gui.UI_BUTTON_PRESSED:  # Обработка нажатий кнопок GUI
                 self.sound_open.play()
-                if event.ui_element == self.gui_manager.button_start:
-                    self.gui_manager.kill_start_menu()
-                    self.start_game1()
-                    self.flag_going_game1 = True
-                elif event.ui_element == self.gui_manager.button_shop:
-                    self.gui_manager.kill_start_menu()
-                    self.flag_going_shop = True
-                    self.gui_manager._load_shop()
-                elif event.ui_element == self.gui_manager.button_setting:
-                    self.gui_manager.kill_start_menu()
-                    self.flag_going_setting = True
-                    self.gui_manager._load_setting()
-                elif event.ui_element == self.gui_manager.button_back:
-                    self.gui_manager.load_start_menu()
-                    self.gui_manager.button_back.kill()
-                    if self.flag_going_game1:
-                        self.gui_manager.exit_game1()
-                    if self.flag_going_setting:
-                        self.gui_manager.exit_setting()
-                        self.progress_bar1_f = self.progress_bar1
-                        self.progress_bar2_f = self.progress_bar2
+                if self.place == "menu": # Обработка меню
+                    if event.ui_element == self.gui_manager.button_start:
+                        self.gui_manager.kill_start_menu()
+                        self.start_game1()
+                        self.flag_going_game1 = True
+                        self.place = 'start1'
+                    elif event.ui_element == self.gui_manager.button_shop:
+                        self.gui_manager.kill_start_menu()
+                        self.flag_going_shop = True
+                        self.gui_manager._load_shop()
+                        self.place = 'shop'
+                    elif event.ui_element == self.gui_manager.button_setting:
+                        self.gui_manager.kill_start_menu()
+                        self.flag_going_setting = True
+                        self.gui_manager._load_setting()
+                        self.place = 'setting'
+                    elif event.ui_element == self.gui_manager.button_back:
+                        self.gui_manager.load_start_menu()
+                        self.gui_manager.button_back.kill()
+                        if self.place == 'game1':
+                            self.gui_manager.exit_game1()
+                            self.flag_going_game1 = False
+                        if self.place == 'setting':
+                            self.gui_manager.exit_setting()
+                            self.progress_bar1_f = self.progress_bar1
+                            self.progress_bar2_f = self.progress_bar2
+                            pygame.mixer.music.set_volume(self.progress_bar1 / 100)
+                            self.sound_open.set_volume(self.progress_bar2 / 100)
+                            self.flag_going_setting = False
+                        if self.place == 'shop':
+                            self.gui_manager.exit_shop()
+                            self.flag_going_shop = False
+                        self.place = 'menu'
+                elif self.place == "setting": # Обработка настроек
+                    if event.ui_element == self.gui_manager.button_minys1: 
+                        if self.progress_bar1_f > 0:
+                            self.gui_manager.progress_bar1_f.set_current_progress(self.progress_bar1_f - 5)
+                            self.progress_bar1_f -= 5
+                            pygame.mixer.music.set_volume(self.progress_bar1_f / 100)
+                    elif event.ui_element == self.gui_manager.button_minys2:
+                        if self.progress_bar2_f > 0:
+                            self.gui_manager.progress_bar2_f.set_current_progress(self.progress_bar2_f - 5)
+                            self.progress_bar2_f -= 5
+                            self.sound_open.set_volume(self.progress_bar2_f / 100)
+                    elif event.ui_element == self.gui_manager.button_plus1:
+                        if self.progress_bar1_f < 100:
+                            self.gui_manager.progress_bar1_f.set_current_progress(self.progress_bar1_f + 5)
+                            self.progress_bar1_f += 5
+                            pygame.mixer.music.set_volume(self.progress_bar1_f / 100)
+                    elif event.ui_element == self.gui_manager.button_plus2:
+                        if self.progress_bar2_f < 100:
+                            self.gui_manager.progress_bar2_f.set_current_progress(self.progress_bar2_f + 5)
+                            self.progress_bar2_f += 5
+                            self.sound_open.set_volume(self.progress_bar2 / 100)
+                    elif event.ui_element == self.gui_manager.button_save:
+                        self.progress_bar1 = self.progress_bar1_f
+                        self.progress_bar2 = self.progress_bar2_f
                         pygame.mixer.music.set_volume(self.progress_bar1 / 100)
                         self.sound_open.set_volume(self.progress_bar2 / 100)
-                    if self.flag_going_shop:
-                        self.gui_manager.exit_shop()
-                    self.flag_going_shop = False
-                    self.flag_going_setting = False
-                    self.flag_going_game1 = False
-                elif event.ui_element == self.gui_manager.button_minys1: # Обработка настроек
-                    if self.progress_bar1_f > 0:
-                        self.gui_manager.progress_bar1_f.set_current_progress(self.progress_bar1_f - 5)
-                        self.progress_bar1_f -= 5
-                        pygame.mixer.music.set_volume(self.progress_bar1_f / 100)
-                elif event.ui_element == self.gui_manager.button_minys2:
-                    if self.progress_bar2_f > 0:
-                        self.gui_manager.progress_bar2_f.set_current_progress(self.progress_bar2_f - 5)
-                        self.progress_bar2_f -= 5
-                        self.sound_open.set_volume(self.progress_bar2_f / 100)
-                elif event.ui_element == self.gui_manager.button_plus1:
-                    if self.progress_bar1_f < 100:
-                        self.gui_manager.progress_bar1_f.set_current_progress(self.progress_bar1_f + 5)
-                        self.progress_bar1_f += 5
-                        pygame.mixer.music.set_volume(self.progress_bar1_f / 100)
-                elif event.ui_element == self.gui_manager.button_plus2:
-                    if self.progress_bar2_f < 100:
-                        self.gui_manager.progress_bar2_f.set_current_progress(self.progress_bar2_f + 5)
-                        self.progress_bar2_f += 5
-                        self.sound_open.set_volume(self.progress_bar2 / 100)
-                elif event.ui_element == self.gui_manager.button_save:
-                    self.progress_bar1 = self.progress_bar1_f
-                    self.progress_bar2 = self.progress_bar2_f
-                    pygame.mixer.music.set_volume(self.progress_bar1 / 100)
-                    self.sound_open.set_volume(self.progress_bar2 / 100)
-                    self.database_manager.update_setting(self.progress_bar1, self.progress_bar2)
-                elif event.ui_element == self.gui_manager.button_buy:
-                   self.database_manager.update_inventory(self.gui_manager.name)
-                   self.gui_manager.update_shop()
-                elif event.ui_element == self.gui_manager.button_weapon_swap:
-                    self.gui_manager.load_swap_weapon()
-                elif event.ui_element == self.gui_manager.button_characters_swap:
-                    self.gui_manager.load_swap_characters()
-
-        except AttributeError:
-            pass
+                        update_setting(self.progress_bar1, self.progress_bar2)
+                elif self.place == "shop": # Обработка магазина
+                    if event.ui_element == self.gui_manager.button_buy:
+                        self.database_manager.update_inventory(self.gui_manager.name)
+                        self.gui_manager.update_shop()
+                elif self.place == 'game1': # Обработка предигравого окна
+                    if event.ui_element == self.gui_manager.button_weapon_swap:
+                        self.gui_manager.load_swap_weapons()
+                        self.place == 'weapon_swap'
+                    elif event.ui_element == self.gui_manager.button_characters_swap:
+                        self.gui_manager.load_swap_characters()
+                        self.place == "characters_swap"
+                elif self.place == "weapon_swap":
+                    if event.ui_element == self.gui_manager.button_weapon2:
+                        pass
+                    if event.ui_element == self.gui_manager.button_weapon3:
+                        pass
+                elif self.place == "characters_swap":
+                    if event.ui_element == self.gui_manager.button_characters2:
+                        pass
+                    if event.ui_element == self.gui_manager.button_characters3:
+                        pass
         if self.flag_going_game:
             self.player.update(event=event, timedelta=self.time_delta, mode='event')
 
@@ -154,16 +171,10 @@ class Game:
     def start_game1(self) -> None:
         self.gui_manager.load_start_game1()
 
-
     def start_game2(self):
         self.flag_going_game = True
         self.player = Character()
         self.player_group.add(self.player)
-
-    
-
-
-
 
 if __name__ == '__main__':
     game = Game()
--- managers.py
+++ managers.py
@@ -4,7 +4,7 @@ import pygame_gui
 from pygame_gui.elements import *
 from constants import SIZE, FILENAME_DATABASE, ID_CHARACTER
 # from initialization_classes import
-from other_functions import get_front_frame_current_characters, get_frame_current_weapon
+from other_functions import get_front_frame_current_characters, get_frame_current_weapon, buy, get_weapon_settings
 import sqlite3
 
 
@@ -54,35 +54,26 @@ class GuiManager:
         # self.image_character = ц(self.image_character, 0, 0.2)
         self.label_image_character.set_image(self.image_character)
         self.label_name_character = UILabel(Rect((490, 600, 100, 30)), text='Рыцарь')
-
         # Weapon menu
         self.label_image_weapon = UILabel(Rect((170, 400, 150, 100)), text='')
         self.image_weapon = get_frame_current_weapon(0)
         self.label_image_weapon.set_image(self.image_weapon)
         self.label_name_weapon = UILabel(Rect((220, 600, 100, 30)), text='Меч')
         self.button_weapon_swap = UIButton(relative_rect=Rect((190, 650, 150, 50)),
-                                text='сменить оружие', manager=self.manager,
-                            )
+                                text='сменить оружие', manager=self.manager, )
         self.button_characters_swap = UIButton(relative_rect=Rect((470, 650, 150, 50)),
-                                text='сменить персонажа', manager=self.manager,
-                            )
+                                text='сменить персонажа', manager=self.manager, )
         self.button_level1 = UIButton(relative_rect=Rect((140, 100, 100, 70)),
-                                text='1 уровень', manager=self.manager,
-                            )
+                                text='1 уровень', manager=self.manager, )
         self.button_level2 = UIButton(relative_rect=Rect((340, 100, 100, 70)),
-                                text='2 уровень', manager=self.manager,
-                            )
+                                text='2 уровень', manager=self.manager, )
         self.button_level3 = UIButton(relative_rect=Rect((540, 100, 100, 70)),
-                                text='3 уровень', manager=self.manager,
-                            )
+                                text='3 уровень', manager=self.manager, )
         self.button_play = UIButton(relative_rect=Rect((290, 250, 200, 70)),
-                                text='ИГРАТЬ', manager=self.manager,
-                            )
-
-
+                                text='ИГРАТЬ', manager=self.manager, )
 
     def _load_setting(self) -> None:
-        progress_bar1, progress_bar2 = self.database_manager.get_characteristics_settings()
+        money, progress_bar1, progress_bar2 = get_weapon_settings()
         self.button_back = UIButton(relative_rect=pygame.Rect(2, 2, 50, 30), text='назад',
                                     manager=self.manager)
         # image_button_back = pygame.image.load('image/back.png')
@@ -109,13 +100,12 @@ class GuiManager:
                                     manager=self.manager)
         self.button_save = UIButton(relative_rect=pygame.Rect(820, 0, 80, 50), text='сохранить',
                                     manager=self.manager)
-
-
+        
     def _load_shop(self) -> None:
         self.button_back = UIButton(relative_rect=pygame.Rect(2, 2, 50, 30), text='назад',
                                     manager=self.manager, )
         shop = self.database_manager.shop()
-        self.label_name_weapon  = UILabel(Rect((150, 400, 60, 30)), text=shop[0][1])
+        self.label_name_weapon  = UILabel(Rect((150, 400, 60, 30)), text=f'{shop[0][1]} - {shop[0][4]}$')
         self.label_image_weapon = UILabel(Rect((100, 180, 150, 100)), text='')
         self.image_weapon = get_frame_current_weapon(shop[0][2])
         self.label_image_weapon.set_image(self.image_weapon)
@@ -127,28 +117,62 @@ class GuiManager:
             self.name = shop[0][1]
         else:
             self.label_image_weapon = UILabel(Rect((150, 450, 60, 30)), text='купленно')
+        myonets = get_weapon_settings()[0]
+        self.label_name_myonets  = UILabel(Rect((150, 50, 50, 30)), text=str(myonets)+"$")
+        self.label_image_myonets = UILabel(Rect((120, 50, 30, 30)), text='')
+        self.image_myonets =  pygame.image.load('image/other/icon-coin.png')
+        self.label_image_myonets.set_image(self.image_myonets)
+
 
     def update_shop(self) -> None:
+        self.button_buy.kill()
         self.label_image_weapon = UILabel(Rect((150, 450, 60, 30)), text='купленно')
-
-    def load_swap_weapon(self) -> None:
-        self.exit_setting()
-        self.button_back = UIButton(relative_rect=pygame.Rect(2, 2, 50, 30), text='назад',
-                                    manager=self.manager, )
-        self.label_image_character = UILabel(Rect((420, 370, 400, 400)), text='')
-        self.image_character = get_front_frame_current_characters()
-        self.label_image_character.set_image(self.image_character)
-        self.label_name_character = UILabel(Rect((490, 600, 100, 30)), text='Рыцарь')
+        self.database_manager.update_inventory(self.name)
+        buy(self.database_manager.buy(self.name))
 
     def load_swap_characters(self) -> None:
         self.exit_setting()
+        f = get_weapon_settings
         self.button_back = UIButton(relative_rect=pygame.Rect(2, 2, 50, 30), text='назад',
                                     manager=self.manager, )
-        self.label_image_weapon = UILabel(Rect((170, 400, 150, 100)), text='')
-        self.image_weapon = get_frame_current_weapon(0)
-        self.label_image_weapon.set_image(self.image_weapon)
-        self.label_name_weapon = UILabel(Rect((220, 600, 100, 30)), text='Меч')
-        
+        self.label_image_characters1 = UILabel(Rect((100, 50, 400, 400)), text=f'')
+        self.image_characters1 = get_front_frame_current_characters(0)
+        self.label_image_characters1.set_image(self.image_weapon1)
+        self.label_name_characters1 = UILabel(Rect((300, 500, 100, 50)), text=f'{f[4]}')
+        self.label_name1  = UILabel(Rect((300, 600, 50, 30)), text='выбрано')
+        characters = self.database_manager.get_character()
+        self.label_image_characters2 = UILabel(Rect((550, 50, 400, 400)), text=f'')
+        self.image_characters2 = get_front_frame_current_characters(1)
+        self.label_image_characters2.set_image(self.image_weapon1)
+        self.label_name_characters2 = UILabel(Rect((750, 500, 100, 50)), text=f'{characters[0][1]}')
+        self.button_characters2  = UIButton(Rect((600, 600, 50, 30)), text='выбрать')
+        self.label_image_characters3 = UILabel(Rect((550, 50, 400, 400)), text=f'')
+        self.image_characters3 = get_front_frame_current_characters(2)
+        self.label_image_characters3.set_image(self.image_weapon1)
+        self.label_name_characters3 = UILabel(Rect((750, 500, 100, 50)), text=f'{characters[1][1]}')
+        self.button_characters3  = UIButton(Rect((600, 600, 50, 30)), text='выбрать')
+
+    def load_swap_weapons(self) -> None:
+        self.exit_setting()
+        f = get_weapon_settings
+        self.button_back = UIButton(relative_rect=pygame.Rect(2, 2, 50, 30), text='назад',
+                                    manager=self.manager, )
+        self.label_image_weapon1 = UILabel(Rect((100, 50, 400, 400)), text=f'')
+        self.image_weapon1 = get_front_frame_current_characters(0)
+        self.label_image_weapon1.set_image(self.image_weapon1)
+        self.label_name_weapon1 = UILabel(Rect((300, 500, 100, 50)), text=f'{f[4]}')
+        self.label_name1  = UILabel(Rect((300, 600, 50, 30)), text='выбрано')
+        characters = self.database_manager.get_character()
+        self.label_image_weapon2 = UILabel(Rect((550, 50, 400, 400)), text=f'')
+        self.image_weapon2 = get_front_frame_current_characters(1)
+        self.label_image_weapon2.set_image(self.image_weapon1)
+        self.label_name_weapon2 = UILabel(Rect((750, 500, 100, 50)), text=f'{characters[0][1]}')
+        self.button_weapon2  = UIButton(Rect((600, 600, 50, 30)), text='выбрать')
+        self.label_image_weapon3 = UILabel(Rect((550, 50, 400, 400)), text=f'')
+        self.image_weapon3 = get_front_frame_current_characters(2)
+        self.label_image_weapon3.set_image(self.image_weapon1)
+        self.label_name_weapon3 = UILabel(Rect((750, 500, 100, 50)), text=f'{characters[1][1]}')
+        self.button_weapon3  = UIButton(Rect((600, 600, 50, 30)), text='выбрать')
 
     def exit_setting(self):
         self.button_fihki.kill()
@@ -161,7 +185,6 @@ class GuiManager:
         self.button_plus1.kill()
         self.button_plus2.kill()
         self.scroll_setting.kill()
-
     
     def exit_game1(self):
         self.label_image_character.kill()
@@ -174,13 +197,15 @@ class GuiManager:
         self.button_level2.kill()
         self.button_level3.kill()
         self.button_play.kill()
-
+        
     def exit_shop(self):
         self.label_image_weapon.kill()
         self.label_name_weapon.kill()
         self.button_buy.kill()
         self.label_image_weapon.kill()
         self.scroll_shop.kill()
+        self.label_image_myonets.kill()
+        self.label_name_myonets.kill()
 
 
 
@@ -201,17 +226,20 @@ class DatabaseManager:
         con.close()
         return result
     
-    def get_characteristics_settings(cls) -> tuple[int, int]:
+    @classmethod
+    def update_inventory(cls, name):
         con, cur = cls._connection_to_database()
-        result = cur.execute('SELECT progress_bar1, progress_bar2 FROM settings ').fetchone()
+        cur.execute("UPDATE Weapons SET purchased = ? WHERE Name = ?", (1, name.lower())) 
+        cur.execute
         con.close()
-        print(result)
-        return result
-    
-    def update_setting(cls, progress_bar1, progress_bar2) -> None:
+
+    def buy(cls, name):
         con, cur = cls._connection_to_database()
-        cur.execute(f"UPDATE settings SET progress_bar1 = {progress_bar1}, progress_bar2 = {progress_bar2}")
+        answer = cur.execute("SELECT purchased WHERE Name = ?", (name.lower())).fetchone()
+        print(answer)
+        cur.execute
         con.close()
+        return answer
 
     def shop(cls):
         con, cur = cls._connection_to_database()
@@ -219,9 +247,16 @@ class DatabaseManager:
         con.close()
         return result
     
-    def update_inventory(cls, name):
+    def get_characters(cls):
         con, cur = cls._connection_to_database()
-        cur.execute(f"UPDATE settings SET purchased = {1} WHERE Name = {name}")
-        cur.execute
+        result = cur.execute('SELECT name, health, damage, speed FROM characters '
+                             'WHERE id=?', (ID_CHARACTER,)).fetchall()
         con.close()
-        
+        return result
+    
+    def get_weapon(cls):
+        con, cur = cls._connection_to_database()
+        result = cur.execute('SELECT Name, filename_image, purchased, price FROM Weapons '
+                             'WHERE id=?', (ID_CHARACTER,)).fetchall()
+        con.close()
+        return result
